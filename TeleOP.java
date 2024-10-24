5141abe407b4a73bcb3e0cedda141ce6c83a1ff8
LacyPage157 <114452244+LacyPage157@users.noreply.github.com>
2024-10-24 16:35:15 -0700

    dtrfgyuhj

---
+++ TeleOP.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.navaigation.CurrentUnit;
+
+@TeleOp(name="FTC Starter Kit Example Robot (INTO THE DEEP)", group="Robot")
+public class TeleOp extends LinearOpMode {
+
+    public DcMotor  leftDrive   = null; //the left drivetrain motor
+    public DcMotor  rightDrive  = null; //the right drivetrain motor
+    public DcMotor  armMotor    = null; //the arm motor
+    public CRServo  intake      = null; //the active intake servo
+    public Servo    wrist       = null; //the wrist servo
+
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1/360.0; // we want ticks per degree, not per rotation
+
+    public void runOpMode() {
+
+    }
+
+
+    
+}detUnit;
+
