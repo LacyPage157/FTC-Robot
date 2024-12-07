8e34f3a3fdbe001becf0c14ce7d4d84eb80f12d0
DanG-777 <drcgast777@gmail.com>
2024-12-07 00:29:30 +0000

    im sorry frank

--- AutoSpecimen.java
+++ AutoSpecimen.java
@@ -9,3 +9,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.external.android;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@Autonomous(name="AutoSpecimen", group="Robot")
+public class AutoSpecimen extends LinearOpMode
+{
+
+
+
+}
+
+public void rotateRobot(double degrees)
+{
+    targetRotations=(434/96)*(degrees/360);
+    rightDrive.setDirection(DcMotor.Direction.FORWARD);
+    runOpMode();
+}
+
+public void moveRobot(double x, double yaw) 
+{
+        // Calculate left and right wheel powers.
+        double leftPower    = x - yaw;
+        double rightPower   = x + yaw;
+}
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+        if (max >1.0) {
+            leftPower /= max;
+            rightPower /= max;
+        }
--- HardCode.java
+++ HardCode.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.external.android;
-import java.lang;
+
 
 @Autonomous(name="HardCode", group="Robot")
 public class HardCode extends LinearOpMode
@@ -20,25 +20,29 @@ public class HardCode extends LinearOpMode
     final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
     final double MAX_AUTO_TURN  = 0.25;  //  Clip the turn speed to this max value (adjust for your robot)
 
-    private double DESIRED_DISTANCE  = null; // Set in inches
+    private double DESIRED_DISTANCE  = 24; // Set in inches
 
 
 
-    private DcMotor leftDrive   = null;  //  Used to control the left drive wheel
-    private DcMotor rightDrive  = null;  //  Used to control the right drive wheel
-    private double basePosLeft = null;
-    private double basePosRight = null;
-    private int rotationsLeft = 0;
-    private int rotationsRight = 0;
+    private DcMotor leftDrive;  //  Used to control the left drive wheel
+    private DcMotor rightDrive;  //  Used to control the right drive wheel
+    private double basePosLeft;
+    private double basePosRight;
+    private double rotationsLeft = 0.0;
+    private double rotationsRight = 0.0;
 
-    private double wheelCircumference = (96*Math.PI)
+    private double wheelCircumference = (96*Math.PI);
     private double targetRotations = DESIRED_DISTANCE/wheelCircumference;
+    
+    
     
 
     @Override public void runOpMode()
     {
         rightDrive = hardwareMap.get(DcMotor.class, "rightMotor");
         leftDrive = hardwareMap.get(DcMotor.class, "leftMotor");
+        leftDrive.setTargetPosition(0);
+        rightDrive.setTargetPosition(0);
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         basePosLeft = leftDrive.getCurrentPosition();
@@ -51,12 +55,16 @@ public class HardCode extends LinearOpMode
 
         
 
-        double drive = Range.clip(1 * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-        double turn  = Range.clip(0 * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+        // double drive = Range.clip(1 * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+        // double turn  = Range.clip(0 * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+        double drive = 1;
+        double turn = 0;
 
             
         leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        waitForStart();
         
 
         while(opModeIsActive()) {
@@ -70,8 +78,8 @@ public class HardCode extends LinearOpMode
             rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rotationsLeft ++;
         }
-        leftDrive.setTargetPosition(targetRotations-rotationsLeft);
-        leftDrive.setTargetPosition(targetRotations-rotationsRight);
+        leftDrive.setTargetPosition((int)targetRotations-(int)rotationsLeft);
+        leftDrive.setTargetPosition((int)targetRotations-(int)rotationsRight);
         if ((targetRotations-(int)targetRotations) == leftDrive.getCurrentPosition()){
             leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -92,7 +100,7 @@ public class HardCode extends LinearOpMode
     }
 
     public void setDesiredDistance(double x){ //X is in inches for consistency
-        DESIRED_DISTANCE = x*25.4 //Convert inches to mm
+        DESIRED_DISTANCE = x*25.4; //Convert inches to mm
     }
 
     public void rotateRobot(double degrees){
@@ -123,46 +131,46 @@ public class HardCode extends LinearOpMode
 }
 
 /*
-DcMotorController	getController()
+DcMotorController    getController()
 Returns the underlying motor controller on which this motor is situated.
 
-int	getCurrentPosition()
+int    getCurrentPosition()
 Returns the current reading of the encoder for this motor.
 
-DcMotor.RunMode	getMode()
+DcMotor.RunMode    getMode()
 Returns the current run mode for this motor
 
-MotorConfigurationType	getMotorType()
+MotorConfigurationType    getMotorType()
 Returns the assigned type for this motor.
 
-int	getPortNumber()
+int    getPortNumber()
 Returns the port number on the underlying motor controller on which this motor is situated.
 
-boolean	getPowerFloat()
+boolean    getPowerFloat()
 Returns whether the motor is currently in a float power level.
 
-int	getTargetPosition()
+int    getTargetPosition()
 Returns the current target encoder position for this motor.
 
-DcMotor.ZeroPowerBehavior	getZeroPowerBehavior()
+DcMotor.ZeroPowerBehavior    getZeroPowerBehavior()
 Returns the current behavior of the motor were a power level of zero to be applied.
 
-boolean	isBusy()
+boolean    isBusy()
 Returns true if the motor is currently advancing or retreating to a target position.
 
-void	setMode(DcMotor.RunMode mode)
+void    setMode(DcMotor.RunMode mode)
 Sets the current run mode for this motor
 
-void	setMotorType(MotorConfigurationType motorType)
+void    setMotorType(MotorConfigurationType motorType)
 Sets the assigned type of this motor.
 
-void	setPowerFloat()
+void    setPowerFloat()
 Deprecated. 
 This method is deprecated in favor of direct use of setZeroPowerBehavior() and setPower().
 
-void	setTargetPosition(int position)
+void    setTargetPosition(int position)
 Sets the desired encoder target position to which the motor should advance or retreat and then actively hold there at.
 
-void	setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior)
+void    setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior)
 Sets the behavior of the motor when a power level of zero is applied.
 */
